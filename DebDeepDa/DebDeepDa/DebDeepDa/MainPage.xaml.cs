902baa5208879848c872ed1079b3fe8aec2c4de6
Ritesh <riteshpzs12@gmail.com>
2019-07-30 04:30:01 +0530

    First Commit For Xamarin Project

---
+++ DebDeepDa/DebDeepDa/DebDeepDa/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DebDeepDa
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            //Demo();
+        }
+
+        async void HOLA(object sender, EventArgs e)
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync("http://192.168.1.27:8081/rit");
+            if (response.IsSuccessStatusCode)
+            {
+                List<MyModel> myModel = JsonConvert.DeserializeObject<List<MyModel>>(GetHttpResponse(response));
+                App.Current.MainPage.DisplayAlert(myModel[0].Username, myModel[0].Imagesrc, myModel[0].Pass);
+            }
+        }
+
+        string GetHttpResponse(HttpResponseMessage httpResponseMessage)
+        {
+            return httpResponseMessage.Content.ReadAsStringAsync().Result;
+        }
+    }
+}
