6efb3fe5c5544428915dacaac949863dcecc42b3
Ritesh <riteshpzs12@gmail.com>
2019-07-30 04:30:01 +0530

    First Commit For Xamarin Project

---
+++ DebDeepDa/DebDeepDa/DebDeepDa/MyModel.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DebDeepDa
+{
+    class MyModel
+    {
+            [JsonProperty("_id")]
+            public string Id { get; set; }
+
+            [JsonProperty("username")]
+            public string Username { get; set; }
+
+            [JsonProperty("pass")]
+            public string Pass { get; set; }
+
+            [JsonProperty("imagesrc")]
+            public string Imagesrc { get; set; }
+
+            [JsonProperty("uid")]
+            public string Uid { get; set; }
+
+            [JsonProperty("manager")]
+            public string Manager { get; set; }
+
+            [JsonProperty("Role")]
+            public string Role { get; set; }
+
+            [JsonProperty("location")]
+            public string Location { get; set; }
+
+            [JsonProperty("geo")]
+            public string Geo { get; set; }
+
+            [JsonProperty("device_id")]
+            public string DeviceId { get; set; }
+
+    }
+}
